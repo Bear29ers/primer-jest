69d368a76d511a8909225e7d3813bbefbdb75f67
Yuta Okuma <y.dream.nest@icloud.com>
2023-03-01 22:49:12 +0900

    refactor: :bulb: refactor eslint setting & setup jest

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,16 +1,35 @@
+/** @type {import('estlint').Linter.Config} */
+
 module.exports = {
   root: true,
   env: {
     browser: true,
     node: true,
     es2021: true,
+    jest: true,
   },
-  extends: ['eslint:recommended', 'plugin:react/recommended', 'plugin:@typescript-eslint/recommended', 'prettier'],
+  extends: [
+    'eslint:recommended',
+    'plugin:@typescript-eslint/recommended',
+    'plugin:import/recommended',
+    'plugin:import/typescript',
+    'plugin:jest/recommended',
+    'prettier',
+  ],
   parser: '@typescript-eslint/parser',
   overrides: [],
+  plugins: ['@typescript-eslint', 'import', 'jest'],
   parserOptions: {
     ecmaVersion: 'latest',
     sourceType: 'module',
+  },
+  settings: {
+    'import/parsers': {
+      '@typescript-eslint/parser': ['.ts', '.tsx'],
+    },
+    'import/resolver': {
+      typescript: true,
+    },
   },
   globals: {},
   rules: {
