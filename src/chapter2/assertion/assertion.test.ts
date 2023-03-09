53df4766d76e47199b2cf5fabe281166820dceaa
Yuta Okuma <y.dream.nest@icloud.com>
2023-03-09 19:55:26 +0900

    feat: :sparkles: toThrow

--- src/chapter2/assertion/assertion.test.ts
+++ src/chapter2/assertion/assertion.test.ts
@@ -359,3 +359,34 @@ test('triggered by the scheduled pipeline', () => {
     }),
   );
 });
+
+// ---------------------------------
+// Errorの評価
+// ---------------------------------
+
+class User {
+  name: string;
+  password: string;
+  constructor({ name, password }: { name: string; password: string }) {
+    // passwordは6文字以下の場合、Errorをthrowする
+    if (password.length < 6) throw new Error('The password length must be at least 6 characters.');
+    this.name = name;
+    this.password = password;
+  }
+}
+
+// パスワードが6文字未満の場合にErrorがthrowされる
+test('creates a new user with a 6-character password', () => {
+  expect(new User({ name: 'hoge', password: '123456' })).toEqual({
+    name: 'hoge',
+    password: '123456',
+  });
+});
+
+test('throw Error when the length of password is less than 6', () => {
+  expect(() => new User({ name: 'hoge', password: '12345' })).toThrow(); // Errorがthrowされたかのチェック
+  expect(() => new User({ name: 'hoge', password: '12345' })).toThrow(Error); // 型のチェック
+  expect(() => new User({ name: 'hoge', password: '12345' })).toThrow(
+    'The password length must be at least 6 characters', // エラーメッセージのチェック
+  );
+});
