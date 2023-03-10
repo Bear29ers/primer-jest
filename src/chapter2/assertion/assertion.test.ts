aaf552f06349299fb63ec6401d1d13cab4d63991
Yuta Okuma <y.dream.nest@icloud.com>
2023-03-10 09:29:04 +0900

    feat: :sparkles: .rejects

--- src/chapter2/assertion/assertion.test.ts
+++ src/chapter2/assertion/assertion.test.ts
@@ -426,3 +426,17 @@ test('return lemon with promise', () => {
 test('return lemon with async/await', async () => {
   await expect(fetchDataWithPromiseResolve()).resolves.toBe('lemon');
 });
+
+// .rejectsを利用した非同期な関数の結果の評価
+const fetchDataWithPromiseReject = () =>
+  new Promise((_, reject) => setTimeout(reject, 1000, new Error('lemon does not exist')));
+
+// .rejectsを利用して失敗時の値を受け取る
+test('failed to return lemon with promise', () => {
+  return expect(fetchDataWithPromiseReject()).rejects.toThrow('lemon does not exist');
+});
+
+// async/awaitを利用
+test('failed to return lemon with async/await', async () => {
+  await expect(fetchDataWithPromiseReject()).rejects.toThrow('lemon does not exist');
+});
