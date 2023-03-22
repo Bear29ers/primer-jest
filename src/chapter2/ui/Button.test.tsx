e680fcb4cf49ffdeec44057b910db61aee3bc02d
Yuta Okuma <y.dream.nest@icloud.com>
2023-03-22 18:41:09 +0900

    feat: :sparkles: ui test with react-testing-library

--- src/chapter2/ui/Button.test.tsx
+++ src/chapter2/ui/Button.test.tsx
@@ -1,4 +1,9 @@
+/**
+ * @jest-environment jsdom
+ */
+
 import renderer from 'react-test-renderer';
+import { render, fireEvent } from '@testing-library/react';
 import { Button } from './Button';
 
 describe('Button', () => {
@@ -6,5 +11,16 @@ describe('Button', () => {
   it('renders correctly with react-test-renderer', () => {
     const button = renderer.create(<Button />);
     expect(button.toJSON()).toMatchSnapshot();
+  });
+
+  // React Testing Libraryを利用した、動きのあるコンポーネントのテスト
+  it('changes the button test upon clicking the button using React Testing Library', () => {
+    const button = render(<Button />);
+    // ボタンをクリックするとONからOFFにボタンの文字列が変化することを確認
+    fireEvent.click(button.getByText('ON'));
+    expect(button.getByText(/OFF/i)).toBeTruthy();
+    // ボタンをクリックするとONからOFFにボタンの文字列
+    fireEvent.click(button.getByText('OFF'));
+    expect(button.getByText(/ON/i)).toBeTruthy();
   });
 });
