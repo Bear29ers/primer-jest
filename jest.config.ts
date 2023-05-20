4a5533d05c842fb70ba5e99fcf040374d2a2c125
Yuta Okuma <y.dream.nest@icloud.com>
2023-05-20 11:27:31 +0900

    feat: :sparkles: upload code coverage

--- jest.config.ts
+++ jest.config.ts
@@ -7,4 +7,21 @@ export default {
   collectCoverage: true,
   coverageReporters: ['text-summary', 'html'],
   coverageDirectory: 'reports/coverage',
+  // コードカバレッジの閾値の設定
+  // coverageThreshold: {
+  //   global: { // 共通で閾値を適用
+  //     branches: 50,
+  //     functions: 50,
+  //     lines: 50,
+  //     statements: 50,
+  //   },
+  //   './src/components/': { // ディレクトリに閾値を適用
+  //     lines: 50,
+  //   },
+  //   './src/reducers/**/*.js': { // ディレクトリの特定のファイルに閾値を適用
+  //     lines: 60,
+  //   },
+  //   './src/api/ver-important-module.js': { // 特定のファイルに閾値を適用
+  //     lines: 100,
+  //   },
 };
